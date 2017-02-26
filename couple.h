8ca3c80509684cccb23da69598dc4ab61bf91fcb
ritikamotwani <iit2015096@iiita.ac.in>
2017-02-27 02:21:01 +0530

    Header file for couple

---
+++ couple.h
@@ -0,0 +1,14 @@
+#ifndef COUPLE_H
+#define COUPLE_H
+#include"boy.h"
+class Couple 
+{
+	public :
+		Girl g;
+		Boy b;
+		int happy;
+		int compatible;
+		void happiness();
+		void compatibility();
+};
+#endif
