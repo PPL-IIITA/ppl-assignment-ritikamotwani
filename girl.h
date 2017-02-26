89bb701f9756310b23dcf65bcbbc34210579b487
ritikamotwani <iit2015096@iiita.ac.in>
2017-02-27 02:02:16 +0530

    Header file for girl

---
+++ girl.h
@@ -0,0 +1,17 @@
+#ifndef GIRL_H
+#define GIRL_H
+class Girl
+{
+	public:
+		int committed;
+		int type;
+		char name[10];
+		int attractive;
+		int intell_g;
+		int maint;
+		int happy;
+		int check;
+		void happiness(int total_cost,int total_value);
+		
+};
+#endif
