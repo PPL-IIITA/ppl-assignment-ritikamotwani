6a86f22ad11d127ebb2acf67053e80cfbdd51f2c
ritikamotwani <iit2015096@iiita.ac.in>
2017-02-27 02:22:47 +0530

    Header file of gift

---
+++ gift.h
@@ -0,0 +1,16 @@
+//!<The header file having all the parameters a gift can have . there are three types of gifts
+#ifndef GIFT_H
+#define GIFT_H
+class Gift
+{
+	public :
+		int value;
+		int price;
+		int type;
+		int which;
+		int luxury_rate;
+		int difficulty;
+		int utility_value;
+		int utility_class;
+};
+#endif
